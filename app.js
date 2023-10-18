605c89c3450644d37d8d4076ed74eac23e6cdff6
AristideI <a.isingizwe@alustudent.com>
2023-10-18 08:55:17 +0200

    updates

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ menuAnchor.forEach((elt) => {
     menuOptions.classList.toggle("display");
     menuOptions.classList.toggle("no-display");
     if (checker) {
-      menuIcon.src = "/img/cross.png";
+      menuIcon.src = "./img/cross.png";
     } else {
-      menuIcon.src = "/img/menu.png";
+      menuIcon.src = "./img/menu.png";
     }
   });
 });
